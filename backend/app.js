b90218e06a6c819e58e6a8395b241cc22d6d46f1
Sai Vinay <vinaysai245@gmail.com>
2025-03-26 04:35:59 +0530

    Latest

--- backend/app.js
+++ backend/app.js
@@ -8,22 +8,14 @@ const userRouter = require("./routes/userRoutes");
 
 app.use(cookieParser());
 const allowedOrigins = [
-  "http://localhost:5001",
+  "http://localhost:5173",
   "https://syncthreads-assgn.vercel.app",
   "https://syncthreads-assgn-p6xb63hns-nanisai245s-projects.vercel.app",
 ];
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://syncthreads-backend-0myw.onrender.com/api",
+  // baseURL: "https://syncthreads-backend-0myw.onrender.com/api",
+  baseURL: import.meta.env.VITE_RENDER_API || "http://localhost:5001/api",
   withCredentials: true,
 });
