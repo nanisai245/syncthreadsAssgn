8f28801ddaa521a1f3331c82398782c6e1c7cae4
Sai Vinay <vinaysai245@gmail.com>
2025-03-25 10:14:21 +0530

    last

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://syncthreads-backend-0myw.onrender.com",
+  baseURL: "https://syncthreads-backend-0myw.onrender.com/api",
   withCredentials: true,
 });
